03deef19ea8c02814aa2a504cb3a45508e978d48
minimyl <aandrew.myles@gmail.com>
2022-11-04 11:16:27 -0400

    changed the navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,18 @@ const Navbar = () => {
     <>
         <div id="Navbar">
         <h1>Juice-box</h1>
-        <div className="navButtons">
-            <button>Register</button>
-            <button><Link to={'/login'}>Login</Link></button>
-            <button>Post</button>
+        <div className="dropdown">
+            <div className="dropbtn">
+                <img 
+                    className="menuimg"
+                    src="https://cdn.icon-icons.com/icons2/916/PNG/512/Menu_icon_icon-icons.com_71858.png"
+                    alt="menu"/>
+            </div>
+            <div className="dropdown-text">
+            <a><Link to={'/register'}>Register</Link></a>
+            <a><Link to={'/login'}>Login</Link></a>
+            <a><Link>Post</Link></a>
+            </div>
         </div>
         </div>
         <div className="Login-Register">
