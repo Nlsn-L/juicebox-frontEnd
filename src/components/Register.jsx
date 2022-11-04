cb05d07380936297d0dfabb9e9d3947ba471b502
minimyl <aandrew.myles@gmail.com>
2022-11-04 11:58:32 -0400

    Added css to register and changed css names around

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,59 +1,64 @@
 import React from "react";
 import { RegisterUser } from "../api";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const Register = (props) => {
   const navigate = useNavigate();
 
   async function handleRegister(e) {
     try {
-     e.preventDefault()
+      e.preventDefault();
 
-    const username = e.target[0].value
-    const password = e.target[1].value
-    const confirmPassword = e.target[2].value
-    const name = e.target[3].value
-    const location = e.target[4].value
+      const username = e.target[0].value;
+      const password = e.target[1].value;
+      const confirmPassword = e.target[2].value;
+      const name = e.target[3].value;
+      const location = e.target[4].value;
 
-    if (password === confirmPassword) {
-      const token = await RegisterUser(
-        username,
-        password,
-        name,
-        location
-      );
-      console.log(token, "this is token")
-      localStorage.removeItem("token");
-      localStorage.setItem("token", token);
-    }
-    else{
-      alert('Passwords do not match')
-    }
-        
-    } catch (error) {
-        
-    }
-    
-
+      if (password === confirmPassword) {
+        const token = await RegisterUser(username, password, name, location);
+        console.log(token, "this is token");
+        localStorage.removeItem("token");
+        localStorage.setItem("token", token);
+      } else {
+        alert("Passwords do not match");
+      }
+    } catch (error) {}
   }
 
   return (
     <div className="box">
-      <form onSubmit={handleRegister}>
-        <label htmlFor="username">Username: </label>
-        <input id="username" type="text" required />
-        <label htmlFor="password">Password: </label>
-        <input id="password" type="password" required />
-        <label htmlFor="Confirm password">Confirm Password: </label>
-        <input id="Confirm password" type="password" required />
-        <label htmlFor="name">Name:</label>
-        <input id="name" type="text" required />
-        <label htmlFor="location">Location: </label>
-        <input id="location" type="text" />
-        <button type="submit"> Submit </button>
+      <h2>Register</h2>
+      <form className="dataInput" onSubmit={handleRegister}>
+        <div className="userBox">
+          <label id="userData">Username: </label>
+          <input id="userInput" type="text" required />
+        </div>
+        <div className="passBox">
+          <label id="userData">Password: </label>
+          <input id="userInput" type="password" required />
+        </div>
+        <div className="confirmBox">
+          <label id="userData">Confirm Password: </label>
+          <input id="userInput" type="password" required />
+        </div>
+        <div className="nameBox">
+          <label id="userData">Name:</label>
+          <input id="userInput" type="text" required />
+        </div>
+        <div className="locationBox">
+          <label id="userData">Location: </label>
+          <input id="userInput" type="text" />
+        </div>
+        <button className="submitBtn" type="submit">
+          Register
+        </button>
       </form>
+      <Link className="signBtn" to={"/login"}>
+        or Sign in
+      </Link>
     </div>
   );
-}
+};
 
 export default Register;
